49669f9277db7048c40a0790d1b8a265f8345d6b
anika253 <148455575+anika253@users.noreply.github.com>
2025-08-28 03:17:17 +0530

    Time: 0 ms (100%), Space: 21.5 MB (34.59%) - LeetHub

--- 0112-path-sum/0112-path-sum.cpp
+++ 0112-path-sum/0112-path-sum.cpp
@@ -15,15 +15,14 @@ public:
         if(root==NULL)
         {
             return false;
+
         }
         targetSum-=root->val;
-        if(targetSum ==0 && root->left ==NULL && root->right ==NULL)
-        {
-            return true;
-        }
-        bool left = hasPathSum(root->left, targetSum);
-        bool right = hasPathSum(root->right, targetSum);
-        return left || right;
-        
+        if(targetSum==0 && root->left ==NULL && root->right ==NULL) return true;
+        bool left= hasPathSum(root->left, targetSum);
+        bool right = hasPathSum(root->right , targetSum);
+        return left||right;
+
+
     }
 };
