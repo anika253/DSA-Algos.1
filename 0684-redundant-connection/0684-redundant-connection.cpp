97666f57167400ad0e0759804b4215fe18eb8847
anika253 <148455575+anika253@users.noreply.github.com>
2025-10-19 09:33:46 +0530

    Time: 4 ms (26%), Space: 13.2 MB (37.4%) - LeetHub

---
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -0,0 +1,74 @@
+class DisjointSet{
+    public:
+    vector<int>rank, parent;
+    DisjointSet(int n)
+    {
+        rank.resize(n+1, 0);
+        parent.resize(n+1);
+        for(int i = 0 ; i<n ; i++)
+        {
+            parent[i]=i;
+        }
+
+    }
+    int findUpar( int node)
+    {
+        if(parent[node]==node)
+        {
+            return node;
+        }
+        else {
+            return parent[node]= findUpar(parent[node]);
+        }
+    }
+    void unionByRank( int u , int v)
+    {
+        int ulp_u = findUpar(u);
+        int ulp_v = findUpar(v);
+        if(rank[ulp_u]> rank[ulp_v])
+        {
+            parent[ulp_v]= ulp_u;
+        }
+        else if( rank[ulp_u]< rank[ulp_v])
+        {
+            parent[ulp_u]= ulp_v;
+
+        }
+        else if( rank[ulp_u]==rank[ulp_v])
+        {
+            parent[ulp_u]= ulp_v;
+            rank[ulp_v]++;
+        }
+    }
+
+};
+
+
+class Solution {
+public:
+
+    vector<int> findRedundantConnection(vector<vector<int>>& edges) {
+        vector<int>ans;
+        int n = edges.size();
+        DisjointSet ds(n);
+        for( auto it: edges)
+        {
+            int u = it[0];
+            int v= it[1];
+            int ulp_u = ds.findUpar(u);
+            int ulp_v = ds.findUpar(v);
+            if(ulp_u == ulp_v)
+            {
+                ans.push_back(u);
+                ans.push_back(v);
+            }
+            else{
+                ds.unionByRank(u , v);
+
+            }
+        }
+        return ans;
+
+        
+    }
+};
