482c166a597e2062c38d03f1d4bb654f9f820c15
anika253 <148455575+anika253@users.noreply.github.com>
2025-10-23 00:58:45 +0530

    Time: 115 ms (19.79%), Space: 184.5 MB (21.98%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,56 +1,60 @@
 class Node{
-    public:
-    int key;
-    int value;
-    Node*next;
-    Node*prev;
-    Node( int key  , int val)
-    {
-        this->key = key;
-        this->val = val;
-      next = NULL;
-        prev = NULL;
-    }
+public:
+int key;
+int value;
+Node*next;
+Node*prev;
+Node(int key ,  int value)
+{
+
+   this->key = key;
+   this->value = value;
+   prev= NULL;
+   next = NULL;
+
+
 }
+
+};
+
 class LRUCache {
 public:
 int capacity;
-unordered_map<int ,Node*>mpp;
+unordered_map<int , Node*>cache;
 Node*head;
 Node*tail;
-LRUCache(int capacity)
-{
-    this->capacity = capacity;
-    head = new Node(-1, -1);
-    tail = new Node(-1, -1);
-    head->next = tail;
-    tail->prev= head;
-}
+    LRUCache(int capacity) {
+        this->capacity= capacity;
+        head = new Node(-1, -1);
+        tail = new Node(-1,-1);
+        head->next = tail;
+        tail->prev= head;
 
-   void add(Node*node)
-   {
-    Node*temp = head->next;
-    head->next = temp;
-    temp->prev = head;
-    node->next = temp;
-    temp->prev= node;
-
-   }
-   void removeNode(Node*node)
-   {
-    Node*prevNode = node->prev;
-    Node*nextnode = node->next;
-    prevNode->next = nextNode;
-    nextNode->prev = prevNode;
-   }
+    }
+    void addNode(Node*node)
+    {
+            Node*temp = head->next;
+            head->next = node;
+            node->prev = head;
+            node->next = temp;
+            temp->prev= node;
+    }
+    void removeNode(Node*node)
+    {
+        Node*prevNode= node->prev;
+        Node*nextNode = node->next;
+        prevNode->next = nextNode;
+        nextNode->prev = prevNode;
+    }
     
     int get(int key) {
         if(cache.find(key)!= cache.end())
         {
-            Node*node cache[key];
+            Node*node = cache[key];
             removeNode(node);
             addNode(node);
             return node->value;
+
         }
         return -1;
     }
@@ -60,19 +64,22 @@ LRUCache(int capacity)
         {
             Node*existing = cache[key];
             removeNode(existing);
+           
             cache.erase(key);
             delete existing;
         }
         if(cache.size()==capacity)
         {
-            Node*tp = tail->prev;
-            removeNode(tp);
-            cache.erase(tp->key);
-            delete tp;
+                Node*tp = tail->prev;
+                removeNode(tp);
+                cache.erase(tp->key);
+                delete tp;
+
+
         }
         Node*nn = new Node(key , value);
         addNode(nn);
-        cache[key]=nm'
+        cache[key]= nn;
     }
 };
 
