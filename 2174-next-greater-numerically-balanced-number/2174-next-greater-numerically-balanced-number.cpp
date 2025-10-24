26dfff5d1816b9e036cb381e9cfab6123366f235
anika253 <148455575+anika253@users.noreply.github.com>
2025-10-24 17:57:13 +0530

    Time: 86 ms (45.73%), Space: 9.1 MB (66.33%) - LeetHub

---
+++ 2174-next-greater-numerically-balanced-number/2174-next-greater-numerically-balanced-number.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool isBalanced(int x) {
+        int cnt[10] = {0};
+        int t = x;
+        while (t > 0) {
+            cnt[t % 10]++;
+            t /= 10;
+        }
+        if (cnt[0] > 0) return false;
+        for (int d = 1; d <= 9; ++d) {
+            if (cnt[d] != 0 && cnt[d] != d) return false;
+        }
+        return true;
+    }
+
+    int nextBeautifulNumber(int n) {
+        int x = n + 1;
+        while (true) {
+            if (isBalanced(x)) return x;
+            ++x;
+        }
+        return -1;
+    }
+};
