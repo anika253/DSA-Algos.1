1c637e0f41d4799d079c66d227be23c4f8dd5192
anika253 <148455575+anika253@users.noreply.github.com>
2025-11-07 15:24:49 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,54 +9,34 @@
 class Solution {
 public:
 ListNode*helper(ListNode*head)
+{ if( head==NULL || head->next ==NULL) return NULL;
+ListNode*slow = head;
+ListNode*fast =head;
+while(fast!= NULL && fast->next!= NULL)
 {
-    if(head==NULL || head->next==NULL)
-        {
-            return NULL;}
-    
-    ListNode* slow = head;
-    ListNode* fast = head;
-    while( fast != NULL && fast->next != NULL)
-    {
-        fast = fast->next->next;
-        slow = slow->next;
-        if( slow==fast)
-        {
-            return slow;
-        }
-    }
+    fast = fast->next->next;
+    slow = slow ->next;
+    if(slow==fast) return slow;
+}
 
-
-        return NULL;
-
+return slow;
 
 }
     ListNode *detectCycle(ListNode *head) {
-        if(head==NULL || head->next==NULL)
-        {
-            return NULL;
-
-
-        }
-        ListNode*inter = helper( head);
-        if( inter==NULL)
-        {
-            return NULL;
-        }
+        if( head==NULL || head->next ==NULL) return NULL;
+        ListNode*inter = helper(head);
+        if( inter==NULL)return NULL;
         ListNode*slow = head;
-        while( slow!= inter)
+        while(slow!= inter)
         {
-         
-            slow = slow ->next;
-               inter = inter->next;
-               
+            slow =slow->next;
+            inter = inter->next;
         }
-        if( slow==inter)
+        if(slow==inter)
         {
             return slow;
         }
         return NULL;
-
-
+        
     }
 };
