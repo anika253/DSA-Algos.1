270cabdd5b02ad65b9e03cba6674354c350a48ab
anika253 <148455575+anika253@users.noreply.github.com>
2025-11-13 22:03:09 +0530

    Time: 18 ms (31.46%), Space: 14.4 MB (14.36%) - LeetHub

--- 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
+++ 0003-longest-substring-without-repeating-characters/0003-longest-substring-without-repeating-characters.cpp
@@ -4,26 +4,26 @@ public:
         int n= s.size();
         int l = 0;
         int r = 0;
-        int maxLen = 0;
-        unordered_map<char , int>mpp;
-        while( r<n)
+        int len = 0;
+        unordered_map<char, int>mpp;
+        while(r<n)
         {
             while(mpp.find(s[r])!= mpp.end())
             {
-                mpp[s[l]]--;
-                if( mpp[s[l]]==0)
-                {
+                 
+                  mpp[s[l]]--;
+                  if(mpp[s[l]]==0)
+                  {
                     mpp.erase(s[l]);
-                }
-                l++;
-                
+                  }
+                  l++;
+
             }
-            mpp[s[r]]++;
-            maxLen = max( maxLen , r-l+1);
-            r++;
-
-
+           len = max( len , r-l+1);
+           mpp[s[r]]++;
+           r++;
+              
         }
-        return maxLen;
+        return len;
     }
 };
