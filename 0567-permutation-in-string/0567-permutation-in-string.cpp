6162fbc625eff41e3fd3b7d03e39fcb2efd21b5e
anika253 <148455575+anika253@users.noreply.github.com>
2025-11-15 16:11:22 +0530

    Time: 1051 ms (5.03%), Space: 60.5 MB (7.38%) - LeetHub

--- 0567-permutation-in-string/0567-permutation-in-string.cpp
+++ 0567-permutation-in-string/0567-permutation-in-string.cpp
@@ -1,41 +1,20 @@
 class Solution {
 public:
-void solve( string s1,int ind ,  vector<string>&ans)
-{
-   int n = s1.size();
-   if(ind==n)
-   {
-    ans.push_back(s1);
-    return;
-   }
-   for(int i = ind ; i<n ; i++)
-   {
-    swap(s1[i], s1[ind]);
-    solve(s1, ind+1, ans);
-    swap(s1[i],s1[ind]);
-
-   }
-   
-
-
-}
     bool checkInclusion(string s1, string s2) {
+        sort(s1.begin(), s1.end());
         int n = s1.size();
         int m = s2.size();
-        if( m<n) return false;
-        vector<int>final;
-        int ind = 0;
-        vector<string>ans;
-        solve( s1, ind, ans);
-        for( int i =0 ; i<ans.size(); i++)
+        if(n>m) return false;
+        for( int i = 0 ; i<= m-n ; i++)
         {
-            if( s2.find(ans[i])!= string ::npos)
-            {
-                return true;
-            }
+           string subs = s2.substr( i , n);
+           sort(subs.begin(), subs.end());
+           if( subs == s1)
+           {
+            return true;
+           }
         }
-return false;
-
-
+        return false;
+        
     }
 };
