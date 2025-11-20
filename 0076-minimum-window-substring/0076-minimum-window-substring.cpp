18770e6bab340f29e08837eb1f1bdba465a93e0a
anika253 <148455575+anika253@users.noreply.github.com>
2025-11-20 23:11:29 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -2,55 +2,47 @@ class Solution {
 public:
     string minWindow(string s, string t) {
         int m = s.size();
-        int n =t.size();
-        vector<int>hash(128 , 0);
-        for( int i =0 ; i<n ; i++)
+        int n = t.size();
+        vector<int>hash(128, 0);
+        for(int i=0 ; i<t.size(); i++)
         {
             hash[t[i]]++;
         }
-    int startI = -1;
-    int l = 0;
-    int r  = 0;
-    int cnt = 0;
-    int maxF = INT_MAX;
-    while(r<m)
-    {
-       if(hash[s[r]]>0)
-       {
-        cnt++;
-       }
-       hash[s[r]]--;
-       while( cnt==n)
-       {
-        if(r-l+1 < maxF)
+        int l= 0;
+        int r = 0;
+        int cnt=0;
+        int startI = -1;
+        int len = 0;
+        while(r<m)
         {
-            maxF = r-l+1;
-            startI = l;
-        } 
-         hash[s[l]]++;
-         if( hash[s[l]]>0)
-         {
-            cnt--;
-         }
-         l++;
-         
+            if(hash[s[r]]>0)
+            {
+                cnt++;
+            }
+            hash[s[r]]--;
+           while(cnt==n)
+           {
+       
+            startI =l;
+            len = max(len, r-l+1);
+          
+            hash[s[l]]++;
+            if(hash[s[l]]>0)
+            {
+                cnt--;
+            }
+            l++;
 
 
 
-       }
 
 
+           }
+   r++;
 
-r++;
-
-
-
-    }
-    if(startI==-1) return "";
-    return s.substr( startI , maxF);
-    
-    
-    
-    
         }
+        if(startI== -1) return "";
+        return s.substr( startI,len);
+    }
+
 };
