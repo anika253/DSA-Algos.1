01b9aca65fd4138f9dc8b2d01b6742f358b27d95
anika253 <148455575+anika253@users.noreply.github.com>
2025-11-30 00:02:35 +0530

    Time: 3 ms (1.71%), Space: 15.1 MB (24.8%) - LeetHub

--- 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
+++ 0019-remove-nth-node-from-end-of-list/0019-remove-nth-node-from-end-of-list.cpp
@@ -10,48 +10,63 @@
  */
 class Solution {
 public:
-int helper(ListNode*head)
+int cnt(ListNode*head)
 {
     if(head==NULL)return 0;
     int cnt = 0;
     ListNode*temp = head;
     while(temp!= NULL)
     {
-        temp = temp->next;
+        temp= temp->next;
         cnt++;
     }
     return cnt;
 }
     ListNode* removeNthFromEnd(ListNode* head, int n) {
         if(head==NULL) return NULL;
-        int l = helper(head);
-        int final = l-n+1;
-        ListNode*temp = head;
-          ListNode*temp2= head;
-          ListNode*prev= NULL;
-        if(final==1)
+        int len = cnt(head);
+        int node_D = len-n+1;
+        if(node_D==1)
         {
-            head= head->next;
-            temp->next = NULL;
-            return head;
+            ListNode*temp= head;
+            head = head->next;
+            temp->next=NULL;
+          
+            delete temp;
+           return head;
+        }
+       else{
+        ListNode*temp = head;
+        ListNode*dummy = new ListNode(-1);
+        for( int i = 1 ; i<node_D-1 ; i++)
+        {
+            temp = temp->next;
+           
 
 
         }
-        else{
-           
-            while(cnt <final && temp2->next!= NULL)
-            {
-             prev= temp;
-             temp = temp->next;
-             cnt++;
+        dummy = temp->next;
+      temp->next= dummy->next;
+      delete dummy;
+      
+
+        
 
             }
-            prev->next = tempe2->next;
-            temp2->next = NULL;
-            delete temp;
 
-        }
-        return head;
-        
+
+
+
+
+
+
+
+
+
+
+
+       return head;
+
+       
     }
 };
