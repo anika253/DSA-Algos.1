72aa7b454b4c83b596973b6a0b2f40ed34bd3c43
anika253 <148455575+anika253@users.noreply.github.com>
2025-12-06 10:43:24 +0530

    Time: 393 ms (47.11%), Space: 11.8 MB (23.86%) - LeetHub

--- 0079-word-search/0079-word-search.cpp
+++ 0079-word-search/0079-word-search.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
 vector<int>dr ={-1, 0 ,  1, 0};
 vector<int>dc ={0, -1, 0 , 1};
-bool helper(vector<vector<char>>& board, string word,vector<vector<int>>&vis, int i , int j, int ind, int m, int n)
-{
-    if(ind == word.size())
+bool solve(vector<vector<char>>& board, string word, vector<vector<int>>&vis, int ind, int i , int j )
+{  int m= board.size();
+        int n = board[0].size();
+    if(ind==word.size())
     {
         return true;
     }
@@ -12,38 +13,31 @@ bool helper(vector<vector<char>>& board, string word,vector<vector<int>>&vis, in
     for( int k = 0 ; k<4 ; k++)
     {
         int nr = i+dr[k];
-        int nc=j+dc[k];
-        if(nr>=0 && nr<m && nc>=0 && nc<n && !vis[nr][nc] && board[nr][nc]== word[ind])
+        int nc = j+dc[k];
+        if(nr>=0 && nr<m && nc>=0 && nc<n && !vis[nr][nc] && board[nr][nc]==word[ind])
         {
-           if (helper(board, word, vis, nr, nc, ind+1,m,n))
-           {
-            return true;
-           }
+            if (solve(board , word, vis , ind+1, nr, nc))return true;
         }
     }
     vis[i][j]=0;
     return false;
-
 }
     bool exist(vector<vector<char>>& board, string word) {
-        int m = board.size();
+        int m= board.size();
         int n = board[0].size();
-        vector<vector<int>>vis(m,vector<int>( n, 0));
-        int ind=1;
-       for( int i = 0 ; i<m ; i++)
-       {
-        for( int j =0;j<n;j++)
+        vector<vector<int>>vis(m , vector<int>( n , 0));
+        int ind = 1;
+        for( int i = 0 ; i<m ; i++)
         {
-            if(board[i][j]==word[0])
+            for( int j = 0 ; j<n ; j++)
             {
-                if(helper(board, word, vis, i ,j  , ind, m, n))
+                if(board[i][j]==word[0])
                 {
-                    return true;
+                   bool a = solve(board , word , vis, ind , i , j);
+                   if(a)return true;
                 }
             }
         }
-       }
-       return false;
-
+        return false;
     }
 };
