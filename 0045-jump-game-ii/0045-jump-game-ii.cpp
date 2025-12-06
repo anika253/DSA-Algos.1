335e22ccf8167a7e34c76f70dd1d9faa8010eab6
anika253 <148455575+anika253@users.noreply.github.com>
2025-12-06 12:41:13 +0530

    Time: 22 ms (27.36%), Space: 20.5 MB (61.64%) - LeetHub

--- 0045-jump-game-ii/0045-jump-game-ii.cpp
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -1,24 +1,21 @@
 class Solution {
 public:
     int jump(vector<int>& nums) {
-        int n = nums.size();
-        int l = 0;
+        int n= nums.size();
+        int l = 0 ;
         int r = 0;
+        int maxJ=0;
         int cnt = 0;
-        int maxJ = 0;
-        while(r<n-1){
-       
-            for( int i = l ;i<=r; i++)
+        while(r<n-1)
+        { 
+            for(int i = l ; i<=r ; i++)
             {
-                maxJ = max(maxJ , i+nums[i]);
+                maxJ = max( maxJ , i+nums[i]);
             }
-            l = r+1;
-            r = maxJ;
+            int l = r+1;
+            r= maxJ;
             cnt++;
-        
-
         }
         return cnt;
-
     }
 };
