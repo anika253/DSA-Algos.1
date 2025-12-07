50ca4b62511251eb5f16019fc39620d1f9d87669
anika253 <148455575+anika253@users.noreply.github.com>
2025-12-07 20:33:09 +0530

    Time: 0 ms (100%), Space: 14.1 MB (53.64%) - LeetHub

--- 0039-combination-sum/0039-combination-sum.cpp
+++ 0039-combination-sum/0039-combination-sum.cpp
@@ -1,44 +1,36 @@
 class Solution {
 public:
-void helper(vector<int>& candidates, int target,  vector<vector<int>>&final,  vector<int>&ans, int ind)
-{ int n = candidates.size();
+void solve(vector<int>& candidates, int target, vector<int>&ds, vector<vector<int>>&final, int ind)
+{    int n = candidates.size();
     if(ind==n)
     {
         if(target==0)
         {
-            final.push_back(ans);
-            
+            final.push_back(ds);
+
         }
         return;
     }
    
-    
-        if(target >= candidates[ind])
-        {
-            ans.push_back( candidates[ind]);
-            helper(candidates, target-candidates[ind], final , ans, ind);// stay at same index;
-            ans.pop_back();
-        }
-        helper(candidates, target, final , ans, ind+1);
-    }
+   if(candidates[ind]<=target)
+   {
+    ds.push_back(candidates[ind]);
+    solve(candidates , target-candidates[ind] , ds, final , ind);
+    ds.pop_back();
+   }
 
 
+  solve(candidates , target , ds, final , ind+1);
 
-
-
-
-
-
-
-
-
-
-    vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
+}
+vector<vector<int>> combinationSum(vector<int>& candidates, int target) {
         int n = candidates.size();
+        vector<int>ds;
         vector<vector<int>>final;
-        vector<int>ans;
-        int ind=0;
-      helper( candidates,target , final , ans , ind);
+        int ind = 0;
+        solve(candidates, target, ds, final , ind);
         return final;
+
+
     }
 };
