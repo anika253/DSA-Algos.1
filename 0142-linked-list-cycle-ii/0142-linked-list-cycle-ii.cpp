b8a5e87a01f70f175588c7cfe34aed663b074858
anika253 <148455575+anika253@users.noreply.github.com>
2025-12-18 13:31:09 +0530

    Time: 11 ms (15.86%), Space: 11.5 MB (21%) - LeetHub

--- 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
+++ 0142-linked-list-cycle-ii/0142-linked-list-cycle-ii.cpp
@@ -9,35 +9,33 @@
 class Solution {
 public:
 ListNode*helper(ListNode*head)
-{ if( head==NULL || head->next ==NULL) return NULL;
+{ if( head==NULL || head->next ==NULL)return NULL;
 ListNode*slow = head;
-ListNode*fast =head;
-while(fast!= NULL && fast->next!= NULL)
-{
-    fast = fast->next->next;
-    slow = slow ->next;
-    if(slow==fast) return slow;
-}
+ListNode*fast = head;
+    while(fast!= NULL && fast->next!= NULL)
+    {  fast = fast->next->next;
+    
+        slow = slow->next;
+      
+        if(slow==fast)return slow;
+    }
+
 
 return NULL;
 
 }
     ListNode *detectCycle(ListNode *head) {
-        if( head==NULL || head->next ==NULL) return NULL;
-        ListNode*inter = helper(head);
-        if( inter==NULL)return NULL;
-        ListNode*slow = head;
-        while(slow!= inter)
-        {
-            slow =slow->next;
-            inter = inter->next;
-        }
-        if(slow==inter)
-        {
-            return slow;
-        }
+        if( head==NULL || head->next ==NULL)return NULL;
         
-        return NULL;
+        ListNode*temp = helper(head);
+        if(temp==NULL)return NULL;
+        ListNode*tmp2 = head;
+        while(temp!= tmp2)
+        {
+            temp= temp->next;
+            tmp2 = tmp2->next;
+        }    
+        return temp;
 
     }
 };
