1f25e1ff3c993bb68def1304c5882277bb21a739
anika253 <148455575+anika253@users.noreply.github.com>
2025-12-21 18:08:31 +0530

    Time: 217 ms (19.21%), Space: 26.1 MB (29.3%) - LeetHub

--- 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
+++ 0005-longest-palindromic-substring/0005-longest-palindromic-substring.cpp
@@ -1,39 +1,44 @@
 class Solution {
 public:
-
-bool isPalindrome(int i , int j , string &s, vector<vector<int>>&dp)
-{
-  if(i>=j)return true;
-  if(dp[i][j]!=-1)return dp[i][j];
-  if(s[i]==s[j])
-  {
-    return isPalindrome(i+1, j-1 , s, dp);
-  }
-
-return dp[i][j]=false;
-
-
-}
     string longestPalindrome(string s) {
-        int n= s.size();
-        int len = INT_MIN;
-        int startI = -1;
-        vector<vector<int>>dp(n , vector<int>( n , -1));
-        for(int i = 0 ; i<n ; i++)
+        int n = s.size();
+        vector<vector<bool>>t(n, vector<bool>(n, 0));
+        int maxL = 1;
+        int startI = 0;
+        for( int i = 0 ; i< n ; i++)
         {
-            for(int j = i ; j<n ; j++)
-            {
-                if(isPalindrome(i, j , s, dp))
-                {
-                    if(len <  j-i+1)
-                    {
-                        len = j-i+1;
-                        startI = i;
-                    }
-                }
-            }
+            t[i][i]=1;
         }
-        if(startI ==-1) return "";
-        return s.substr( startI , len);
+     for( int len =2 ; len <=n ; len++)
+     {
+        for( int i = 0 ; i<n-len+1; i++)
+        {
+            int j = i+len-1;
+            if(s[i]==s[j] && len==2)
+            {
+                t[i][j]= 1;
+                maxL = 2;
+                startI = i;
+            }
+            else if(s[i]==s[j] && t[i+1][j-1])
+            {
+                t[i][j]= 1;
+                if(j-i+1 > maxL)
+                {
+                    maxL = j-i+1;
+                    startI = i;
+
+                }
+                
+            }
+            else{
+                t[i][j]= false;
+
+            }
+
+        }
+
+     }
+return s.substr(startI , maxL);
     }
 };
