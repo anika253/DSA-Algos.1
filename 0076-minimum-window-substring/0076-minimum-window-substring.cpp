5dc69396be718ad3cc68a0e2f7dcc0f1e896410c
anika253 <148455575+anika253@users.noreply.github.com>
2025-12-21 18:38:33 +0530

    Time: 3 ms (79.79%), Space: 11.4 MB (62.33%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.cpp
+++ 0076-minimum-window-substring/0076-minimum-window-substring.cpp
@@ -1,48 +1,51 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int m = s.size();
-        int n = t.size();
-        vector<int>hash(128, 0);
-        for(int i=0 ; i<t.size(); i++)
+        int n= s.size();
+        int m= t.size();
+        if(m >n)return "";
+        unordered_map<char , int>hash;
+        for( auto it:t)
         {
-            hash[t[i]]++;
+            hash[it]++;
         }
-        int l= 0;
-        int r = 0;
-        int cnt=0;
-        int startI = -1;
         int len = INT_MAX;
-        while(r<m)
+        int l =0;
+        int r = 0;
+        int cnt = 0;
+        int startI = -1;
+        while(r<n)
         {
             if(hash[s[r]]>0)
             {
                 cnt++;
             }
             hash[s[r]]--;
-           while(cnt==n)
+      while(cnt==m)
+      {
+             if(r-l+1<len)
+             {
+                len = r-l+1;
+                startI = l;
+             }
+            
+           hash[s[l]]++;
+           if(hash[s[l]]>0)
            {
-          if(r-l+1 <len){
-            startI =l;
-            len =r-l+1;
-          }
-            hash[s[l]]++;
-            if(hash[s[l]]>0)
-            {
-                cnt--;
-            }
-            l++;
-
-
-
-
-
+            cnt--;
            }
-   r++;
+           l++;
+            
+    
+
+
+      }
+
+      r++;
 
         }
-        if(startI== -1) return "";
-        return s.substr( startI,len);
-    }
+        if(startI ==-1)return "";
+        return s.substr(startI , len);
 
+    }
 };
