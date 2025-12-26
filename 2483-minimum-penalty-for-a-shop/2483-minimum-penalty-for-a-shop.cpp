a0e5de57d4c3179f39db2c136863876b6f466387
anika253 <148455575+anika253@users.noreply.github.com>
2025-12-26 22:52:53 +0530

    Time: 23 ms (8.2%), Space: 25.8 MB (5.79%) - LeetHub

--- 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
+++ 2483-minimum-penalty-for-a-shop/2483-minimum-penalty-for-a-shop.cpp
@@ -2,31 +2,46 @@ class Solution {
 public:
     int bestClosingTime(string customers) {
         int n = customers.size();
-
-        vector<int> yes(n + 1, 0);  
-        vector<int> no(n + 1, 0);  
-
+        vector<int>yes(n+1, 0);
+        vector<int>no(n+1, 0);
         
-        for (int i = n - 1; i >= 0; i--) {
-            yes[i] = yes[i + 1] + (customers[i] == 'Y');
+        int y = 0;
+        int n1= 0;
+        for(int i = n-1; i>=0 ; i--)
+        {
+            if(customers[i]=='Y')
+            {
+                y++;}
+                yes[i]=y;
+            
         }
+      
 
-       
-        for (int i = 0; i < n; i++) {
-            no[i + 1] = no[i] + (customers[i] == 'N');
+        for(int i =0 ; i<n ; i++)
+        {
+            if(customers[i]=='N')
+            {
+                n1++;}
+                no[i+1]=n1;
+            
+
         }
+      
 
-        int minPenalty = INT_MAX;
-        int ans = 0;
+        vector<int>ans(n+1, 0);
+      int final = INT_MAX;
+      int bh = 0;
+        for(int i = 0 ; i<=n ; i++)
+        {
+            ans[i]= yes[i]+no[i];
+           if(ans[i]<final)
+           {
+            final = min(final , ans[i]);
+            bh = i;
+           }
 
-        for (int i = 0; i <= n; i++) {
-            int penalty = no[i] + yes[i];
-            if (penalty < minPenalty) {
-                minPenalty = penalty;
-                ans = i;
-            }
+            
         }
-
-        return ans;
+        return bh;
     }
 };
