b38775a57be4d1893530588ceb79f0fc549ec625
anika253 <148455575+anika253@users.noreply.github.com>
2025-12-28 19:59:02 +0530

    Time: 5 ms (9.69%), Space: 17.8 MB (23.59%) - LeetHub

--- 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
+++ 0152-maximum-product-subarray/0152-maximum-product-subarray.cpp
@@ -8,7 +8,7 @@ public:
         for(int i =0 ; i<n ; i++)
         {
             if(pre==0)pre=1;
-            if(suff=0)suff=1;
+            if(suff==0)suff=1;
             pre*= nums[i];
             suff*= nums[n-1-i];
 ans = max(ans , max(pre , suff));
