82e01eedb97511ca287a4818810f4f50167ad84a
anika253 <148455575+anika253@users.noreply.github.com>
2026-01-01 19:33:51 +0530

    Time: 47 ms (34.02%), Space: 75.8 MB (7.67%) - LeetHub

--- 0148-sort-list/0148-sort-list.cpp
+++ 0148-sort-list/0148-sort-list.cpp
@@ -10,47 +10,63 @@
  */
 class Solution {
 public:
-   
-    ListNode* merge(ListNode*left,ListNode* right){
-        ListNode* dummy=new ListNode(-1);
-        ListNode* temp=dummy;
-        while(left!=NULL && right!=NULL){
-            if(left->val<=right->val){
-                temp->next=left;
-                temp = temp->next;
-                left=left->next;
-
-            }
-            else{
-                temp->next=right;
-                temp=temp->next;
-                right=right->next;
-            }
-        }
-        if(left!=NULL){
-            temp->next=left;
-        }
-        if(right!=NULL){
-            temp->next=right;
-        }
-        return dummy->next;
+ListNode*middle(ListNode*head)
+{
+    if(head==NULL ||head->next ==NULL)return head;
+    ListNode*slow = head;
+    ListNode*fast = head->next;
+    while(fast!= NULL && fast->next!= NULL)
+    {
+        fast = fast->next->next;
+        slow = slow->next;
     }
-    ListNode* sortList(ListNode* head) {
-        if(head==NULL || head->next==NULL) return head;
-        ListNode* temp=head;
-        ListNode*slow=head;
-        ListNode* fast=head;
-        while(fast!=NULL && fast->next!=NULL){
-            temp=slow;
-            fast=fast->next->next;
-            slow=slow->next;
-            
+    return slow;
+}
+ListNode*ms(ListNode* left, ListNode* right)
+{
+    if(left==NULL)return right;
+    if(right ==NULL)return left;
+    ListNode*dummy = new ListNode(-1);
+    ListNode*temp = dummy;
+    while(left!= NULL && right != NULL)
+    {
+        if(left->val <= right->val)
+        {
+            temp->next = left;
+            left = left->next;
         }
-        temp->next=NULL;
-        ListNode* l=sortList(head);
-        ListNode*r = sortList(slow);
+        else{
+            temp->next = right;
+            right = right->next;
+        }
+        temp = temp->next;
+    }
+    if(left)
+    {
+        temp->next = left;
+
+    }
+    else if( right)
+    {
+        temp->next = right;
+    }
+    return dummy->next;
+
+}
+    ListNode* sortList(ListNode* head) {
+        if(head==NULL ||head->next ==NULL)return head;
+        ListNode*mid= middle(head);
+       
+
+        ListNode*left = head;
+        ListNode*right = mid->next;
+        mid->next = NULL;
         
-        return merge(l,r);
-        
+
+        left = sortList(left);
+        right = sortList(right);
+      return   ms(left, right );
+
+
     }
 };
