94bf57639a313fe2add59a17fec482335d788033
anika253 <148455575+anika253@users.noreply.github.com>
2026-01-13 23:07:43 +0530

    Time: 0 ms (100%), Space: 27.2 MB (28.62%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.cpp
+++ 0042-trapping-rain-water/0042-trapping-rain-water.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int trap(vector<int>& nums) {
-        int n= nums.size();
-        vector<int>mxl( n, 0);
-        vector<int>mxr( n, 0);
-        mxl[0]= nums[0];
-        mxr[n-1]= nums[n-1];
-        for( int i = 1; i<n; i++)
+    int trap(vector<int>& height) {
+        int n = height.size();
+        vector<int>mxl(n , 0);
+        vector<int>mxr(n , 0);
+        mxl[0]  = height[0];
+        mxr[n-1] = height[n-1];
+        for(int i = 1; i<n ; i++)
         {
-            mxl[i]= max(nums[i], mxl[i-1]);
+            mxl[i] = max( height[i],mxl[i-1] );
         }
-        for( int i = n-2 ; i>=0 ; i--)
+        for(int i = n-2 ; i>=0 ; i--)
         {
-            mxr[i]= max(nums[i], mxr[i+1]);
+            mxr[i]= max(height[i], mxr[i+1]);
         }
         int water = 0;
-        for( int i = 0 ; i<n; i++)
+        for( int i =0 ; i<n; i++)
         {
-            water+= abs(min(mxl[i], mxr[i])-nums[i]);
+            water+= abs(min(mxl[i], mxr[i])-height[i]);
         }
         return water;
     }
