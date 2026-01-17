0d863a332416633c59c74df54d2d4e1464aa6cf5
anika253 <148455575+anika253@users.noreply.github.com>
2026-01-17 14:59:33 +0530

    Time: 0 ms (100%), Space: 13.3 MB (34.02%) - LeetHub

--- 0684-redundant-connection/0684-redundant-connection.cpp
+++ 0684-redundant-connection/0684-redundant-connection.cpp
@@ -1,74 +1,73 @@
 class DisjointSet{
-    public:
-    vector<int>rank, parent;
-    DisjointSet(int n)
+public:
+vector<int>rank, parent;
+DisjointSet( int n)
+{
+    rank.resize(n+1, 0);
+    parent.resize(n+1);
+    for(int i = 0 ; i<n; i++)
     {
-        rank.resize(n+1, 0);
-        parent.resize(n+1);
-        for(int i = 0 ; i<n ; i++)
-        {
-            parent[i]=i;
-        }
+        parent[i]=i;
+    }
+}
+int findUpar(int node)
+{
+    if(parent[node]==node)
+    {
+        return node;
+    }
+    return parent[node]= findUpar(parent[node]);
+}
+void unionByRank(int u , int v)
+{
+    int ulp_u = findUpar( u);
+    int ulp_v = findUpar( v);
+    if(rank[ulp_u]> rank[ulp_v])
+    {
+        parent[ulp_v]=ulp_u;
+
 
     }
-    int findUpar( int node)
+     else if(rank[ulp_u]< rank[ulp_v])
     {
-        if(parent[node]==node)
-        {
-            return node;
-        }
-        else {
-            return parent[node]= findUpar(parent[node]);
-        }
-    }
-    void unionByRank( int u , int v)
-    {
-        int ulp_u = findUpar(u);
-        int ulp_v = findUpar(v);
-        if(rank[ulp_u]> rank[ulp_v])
-        {
-            parent[ulp_v]= ulp_u;
-        }
-        else if( rank[ulp_u]< rank[ulp_v])
-        {
-            parent[ulp_u]= ulp_v;
+        parent[ulp_u]=ulp_v;
 
-        }
-        else if( rank[ulp_u]==rank[ulp_v])
-        {
-            parent[ulp_u]= ulp_v;
-            rank[ulp_v]++;
-        }
+
     }
+    else if(rank[ulp_u]== rank[ulp_v])
+    {
+        parent[ulp_u]=ulp_v;
+        rank[ulp_v]++;
+
+
+    }
+}
+
 
 };
 
 
 class Solution {
 public:
-
     vector<int> findRedundantConnection(vector<vector<int>>& edges) {
-        vector<int>ans;
-        int n = edges.size();
+        int  n = edges.size();
         DisjointSet ds(n);
+        vector<int>ans;
         for( auto it: edges)
         {
             int u = it[0];
-            int v= it[1];
+            int v = it[1];
             int ulp_u = ds.findUpar(u);
-            int ulp_v = ds.findUpar(v);
-            if(ulp_u == ulp_v)
+            int ulp_v= ds.findUpar(v);
+            if(ulp_v==ulp_u)
             {
                 ans.push_back(u);
                 ans.push_back(v);
             }
-            else{
-                ds.unionByRank(u , v);
-
-            }
+      else{
+        ds.unionByRank(u, v);
+      }
         }
         return ans;
-
-        
     }
 };
