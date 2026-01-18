7a72a0ebbd629e669c789d32c6d854ddccfb014a
anika253 <148455575+anika253@users.noreply.github.com>
2026-01-18 12:05:41 +0530

    Time: 173 ms (5.06%), Space: 183.9 MB (20.49%) - LeetHub

--- 0146-lru-cache/0146-lru-cache.cpp
+++ 0146-lru-cache/0146-lru-cache.cpp
@@ -1,8 +1,8 @@
 class LRUCache {
 public:
 list<int>dll;
-map<int , pair<list<int>::iterator, int>>cache;
 int capacity;
+map<int , pair<list<int>::iterator , int>>cache;
     LRUCache(int capacity) {
         this->capacity = capacity;
     }
@@ -11,30 +11,34 @@ int capacity;
         dll.erase(cache[key].first);
         dll.push_front(key);
         cache[key].first = dll.begin();
+
     }
     int get(int key) {
         if(!cache.count(key))return -1;
-      
+
         maker(key);
-          return cache[key].second;
+        return cache[key].second;
+
     }
     
     void put(int key, int value) {
         if(cache.count(key))
         {
-            cache[key].second= value;
             maker(key);
+            cache[key].second= value;
         }
         else{
             dll.push_front(key);
-            cache[key]={dll.begin(), value};
+            cache[key]= {dll.begin(), value};
             capacity--;
         }
-        if(capacity < 0)
+        if(capacity <0)
         {
+            
             cache.erase(dll.back());
             dll.pop_back();
             capacity++;
+
         }
     }
 };
