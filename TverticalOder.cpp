8497c3fe3ba1bcb8ae5a48e70626c8609dec18c9
anika253 <148455575+anika253@users.noreply.github.com>
2026-01-21 00:06:38 +0530

    Refactor VerticalOrder function and main method

--- TverticalOder.cpp
+++ TverticalOder.cpp
@@ -1,89 +1,73 @@
 #include <iostream>
 #include <vector>
-#include<unordered_map>
+#include <map>
 #include <queue>
 using namespace std;
-class TreeNode{
-    public:
+
+class TreeNode {
+public:
     int val;
-    TreeNode*left;
-    TreeNode*right;
-    TreeNode(int x)
-    {
-        this->val = x;
-        this->left= NULL;
-        this->right = NULL;
+    TreeNode* left;
+    TreeNode* right;
+
+    TreeNode(int x) {
+        val = x;
+        left = right = NULL;
     }
 };
-vector<vector<int>>VerticalOrder(TreeNode*root)
+
+vector<vector<int>> VerticalOrder(TreeNode* root)
 {
-  vector<vector<int>>ans;
-    if(!root) return ans;
-    unordered_map<int, vector<int>>mpp;
-    queue<pair<TreeNode*, int>>q;
-    q.push({root, 0});
-    while(!q.empty())
+    vector<vector<int>> ans;
+    if (!root) return ans;
+
+    map<int, vector<int>> mpp;   // ordered
+    queue<pair<TreeNode*, int>> q;
+
+    q.push(make_pair(root, 0));
+
+    while (!q.empty())
     {
-        auto [node, lvl]= q.front();
+        pair<TreeNode*, int> temp = q.front();
         q.pop();
+
+        TreeNode* node = temp.first;
+        int lvl = temp.second;
+
         mpp[lvl].push_back(node->val);
-        if(node->left)
-        {
-            q.push({node->left, lvl-1});
-        }
-        if(node->right)
-        {
-            q.push({node->right, lvl+1});
-        }
-        
-        
-        
-        
+
+        if (node->left)
+            q.push(make_pair(node->left, lvl - 1));
+
+        if (node->right)
+            q.push(make_pair(node->right, lvl + 1));
     }
-    for(auto [nodeV, value]: mpp)
+
+    for (auto it = mpp.begin(); it != mpp.end(); it++)
     {
-        ans.push_back(value);
+        ans.push_back(it->second);
     }
+
     return ans;
 }
-    
-    
-    
-    
-    
-    
 
 int main()
 {
-    TreeNode*root = new TreeNode(1);
+    TreeNode* root = new TreeNode(1);
     root->left = new TreeNode(2);
-     root->right = new TreeNode(3);
-     root->left->right = new TreeNode(4);
-     root->right->left = new TreeNode(5);
-     root->right->right = new   TreeNode(6);
-     vector<vector<int>>ans = VerticalOrder(root);
-     for(int i = 0 ; i<ans.size(); i++)
-     {
-         for(int j = 0 ; j<ans[i].size(); j++)
-         {
-             cout << ans[i][j]<< " ";
-         }
-         cout << endl;
-     }
-     return 0;
-    
+    root->right = new TreeNode(3);
+    root->left->right = new TreeNode(4);
+    root->right->left = new TreeNode(5);
+    root->right->right = new TreeNode(6);
+
+    vector<vector<int>> ans = VerticalOrder(root);
+
+    for (int i = 0; i < ans.size(); i++)
+    {
+        for (int j = 0; j < ans[i].size(); j++)
+        {
+            cout << ans[i][j] << " ";
+        }
+        cout << endl;
+    }
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
