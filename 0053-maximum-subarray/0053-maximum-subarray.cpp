a341f27a7a09f432b0ed1ce89156c8b5cab045d9
anika253 <148455575+anika253@users.noreply.github.com>
2026-01-21 00:23:55 +0530

    Time: 0 ms (100%), Space: 71.7 MB (80.11%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.cpp
+++ 0053-maximum-subarray/0053-maximum-subarray.cpp
@@ -3,17 +3,16 @@ public:
     int maxSubArray(vector<int>& nums) {
         int n = nums.size();
         int sum = 0;
-        int ans = INT_MIN;
-        for( int i =0 ;i<n ; i++)
+        int ans= INT_MIN;
+
+        for(int i =0 ; i<n ; i++)
         {
-            if(sum<0)
-            {
-                sum=0;
-            }
+            
+            if(sum<0) sum = 0;
             sum+= nums[i];
-            ans = max(ans , sum);
+
+            ans= max(ans , sum);
         }
         return ans;
-
     }
 };
