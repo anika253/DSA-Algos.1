6010ecd9147dd03dae0a44b70c1311dfddf6adb6
anika253 <148455575+anika253@users.noreply.github.com>
2026-01-21 22:56:07 +0530

    Time: 47 ms (67.26%), Space: 29.1 MB (45.3%) - LeetHub

--- 0015-3sum/0015-3sum.cpp
+++ 0015-3sum/0015-3sum.cpp
@@ -5,42 +5,36 @@ public:
         vector<vector<int>>final;
         vector<int>ans;
         sort(nums.begin(), nums.end());
-        for( int i = 0; i<n ; i++)
+        for( int i = 0 ; i<n ; i++)
         {
-            if(i>0 && nums[i]==nums[i-1]) continue;
             int j = i+1;
             int k = n-1;
-            while(j<k)
+          if(i>0 && nums[i]==nums[i-1])continue;
+          while(j<k)
+          {
+            int sum = nums[i]+nums[j]+nums[k];
+            if(sum>0)
             {
-                int sum = nums[i]+nums[j]+nums[k];
-                if(sum >0)
-                {
-                    k--;
-                }
-                else if(sum <0)
-                {
-                    j++;
-                }
-                else{
-                   ans.push_back(nums[i]);
-                       ans.push_back(nums[j]);
-                  
-                      ans.push_back(nums[k]);
-                      final.push_back(ans);
-                      ans.clear();
-                       j++;
-                       k--;
-                       while( j<k && nums[j]==nums[j-1])j++;
-                       while(j<k && nums[k]==nums[k+1])k--;
-                  
-                    
-                  
-                    
-                  
-
-                }
+                k--;
             }
+            else if( sum<0)
+            {
+                j++;
+            }
+            else{
+                ans.push_back(nums[i]);
+                         ans.push_back(nums[j]);
+                                  ans.push_back(nums[k]);
+                                  final.push_back(ans);
+                                  ans.clear();
+                                  j++;
+                                  k--;
+                                  while(j<k && nums[j]==nums[j-1])j++;
+                                  while(j<k && nums[k]==nums[k+1])k--;
+
+            }
+          }
         }
-        return final;
+  return final;
     }
 };
