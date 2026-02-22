ec297e8682b5b1ddd91fdc76e8052bb97ceb0826
anika253 <148455575+anika253@users.noreply.github.com>
2026-02-22 14:51:34 +0530

    Time: 0 ms (100%), Space: 8 MB (44.66%) - LeetHub

---
+++ 0868-binary-gap/0868-binary-gap.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int binaryGap(int n) {
+        int ans = 0, last = -1;
+
+        for (int i = 0; n > 0; i++, n >>= 1) {
+            if (n & 1) {
+                if (last != -1)
+                    ans = max(ans, i - last);
+                last = i;
+            }
+        }
+        return ans;
+    }
+};
