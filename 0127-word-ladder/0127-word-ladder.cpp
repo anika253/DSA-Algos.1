52079e6119fd40fdbb3f2a9dd53fc9a5e7b89d8e
anika253 <148455575+anika253@users.noreply.github.com>
2026-06-02 23:30:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0127-word-ladder/0127-word-ladder.cpp
+++ 0127-word-ladder/0127-word-ladder.cpp
@@ -1,35 +1,43 @@
 class Solution {
 public:
     int ladderLength(string beginWord, string endWord, vector<string>& wordList) {
-        int n = wordList.size();
-        unordered_set<string>st(wordList.begin(), wordList.end());
-        queue<pair<string , int>>q;
+        unordered_set<string>st;
+        st.insert(wordList.begin(), wordList.end());
+        queue<pair<string, int>>q;
         q.push({beginWord, 1});
         st.erase(beginWord);
+        int steps =0;
         while(!q.empty())
         {
             auto topo = q.front();
             q.pop();
+            
+            
             string s = topo.first;
-            int steps = topo.second;
-            if(s==endWord)return steps;
-            for(int i = 0; i<s.size(); i++)
-            {
-                char temp= s[i];
-                for(char ch = 'a' ; ch<='z'; ch++)
-                {
-                    s[i]= ch;
-                    if(st.find(s)!= st.end())
-                    {
-                      q.push({s, steps+1});
-                      st.erase(s);
-                    }
+            int final = topo.second;
+            if(s==endWord)return final;
 
+        
+            for( int i = 0 ; i<s.size(); i++)
+            {
+                char temp  =s[i];
+                for( char  ch = 'a'; ch<='z'; ch++)
+                {
+                      s[i]= ch;
+                      if(st.find(s)!= st.end())
+                      {
+                        q.push({s, final+1});
+                        st.erase(s);
+                      }
                 }
-                s[i]= temp;
+                s[i]=temp;
             }
         }
-        return 0;
+        
+         
+
+return 0;
+
 
     }
 };
