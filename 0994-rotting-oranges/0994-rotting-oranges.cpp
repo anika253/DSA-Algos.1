91a4dcf511f00c46ecd927a58186b70e7f897e9b
anika253 <148455575+anika253@users.noreply.github.com>
2026-09-13 14:45:28 +0530

    Time: 0 ms (100%), Space: 17.1 MB (38.88%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.cpp
+++ 0994-rotting-oranges/0994-rotting-oranges.cpp
@@ -1,53 +1,56 @@
 class Solution {
 public:
-vector<int>dr ={-1, 0 , 1, 0};
-vector<int>dc ={0,1 , 0, -1};
+vector<int>dr= {-1,0,1,0};
+vector<int>dc= {0 , 1, 0 ,-1};
     int orangesRotting(vector<vector<int>>& grid) {
         int m = grid.size();
-        int n= grid[0].size();
-        int cntF = 0;
-        int cnt= 0;
-        queue<pair<int , pair<int , int>>>q;
+        int n = grid[0].size();
+        int cntF=0;
+        int cnt = 0;
+        int time = 0;
         vector<vector<int>>vis( m , vector<int>(n, 0));
-       for(int i =0 ; i<m ; i++)
-       {
-        for(int j = 0; j<n; j++)
+        queue<pair<int , pair<int, int>>>q;
+        for(int i = 0; i<m ; i++)
         {
-            if(grid[i][j]==1)
+            for( int j = 0; j<n ; j++)
             {
-
-               cntF++;
-            }
-           else if(grid[i][j]==2)
-           {
-            q.push({0 , {i, j}});
-            vis[i][j]=1;
-           }
+                if(grid[i][j]==1)
+                {
+                    cntF++;
+                    
+                }
             
+            else if(grid[i][j]==2)
+            {
+                q.push({0, {i, j}});
+                vis[i][j]=1;
+            }
         }
-       }
-       int time=0;
-       while(!q.empty())
-       {
-        auto topo = q.front();
-        time = topo.first;
-        int row = topo.second.first;
-        int col = topo.second.second;
-        q.pop();
-        for( int i =0 ; i<4; i++)
-        {
-        int nr = row+dr[i];
-        int nc = col+dc[i];
-        if(nr>=0 && nr<m && nc>=0 && nc<n && !vis[nr][nc] && grid[nr][nc]==1)
-        {
-            q.push({time+1, {nr, nc}});
-            cnt++;
-            vis[nr][nc]=1;
         }
-       }
-       }
-       if(cnt!= cntF)return -1;
-       return time;
+             while(!q.empty())
+             {
+                auto topo = q.front();
+                q.pop();
+                time = topo.first;
+                int r = topo.second.first;
+                int c = topo.second.second;
+                for(int x =0 ; x<4; x++)
+                {
+                    int nr = r+dr[x];
+                    int nc = c+dc[x];
+                  if(nr>=0 && nr<m && nc>=0 && nc<n && !vis[nr][nc] && grid[nr][nc]==1)
+                  {
+                    q.push({time+1, {nr, nc}});
+                    vis[nr][nc]=1;
+                      cnt++;
+                  }
 
+
+                }
+
+             
+             }
+             if(cnt!=cntF)return -1;
+        return time;
     }
 };
